971eba9df6892f8212a99c39960fdaad8208a053
jonathan.rubstein <jrubstein@jonathans-mbp.speedport.ip>
2018-05-26 20:54:03 +0200

    Change version for docker image

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | sed 's/ //g')
+IMAGE_NAME=jrubstein/projecti:$PACKAGE_VERSION
+
+if [ $TRAVIS_BRANCH"x" != "masterx" ]; then
+    IMAGE_NAME=jrubstein/projecti:$TRAVIS_COMMIT
+fi
+
+echo "Building docker image"
+docker build -t $IMAGE_NAME .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jrubstein/projecti:$TRAVIS_COMMIT
+echo "Pushing docker image"
+docker push $IMAGE_NAME
